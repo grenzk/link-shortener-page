3403b327768f94a158fb3ca2086540db25a9cf20
grenzk <74033991+grenzk@users.noreply.github.com>
2023-11-19 17:31:53 +0800

    Create a header component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
-<script setup></script>
+<script setup>
+import BrandLogo from '@/components/images/BrandLogo.vue'
+</script>
 
 <template>
+  <header class="header">
+    <div class="container">
+      <a href="#"><BrandLogo /></a>
+
+      <nav class="nav">
+        <ul>
+          <a href="#"><li>Feaures</li></a>
+          <a href="#"><li>Pricing</li></a>
+          <a href="#"><li>Resources</li></a>
+        </ul>
+      </nav>
+
+      <nav class="nav">
+        <a href="#">Login</a>
+        <a href="#">Sign Up</a>
+      </nav>
+    </div>
+  </header>
   <main>
     <h1>Hello, Vue!</h1>
   </main>
